4420016ab85ae8dc3e61fd19ad65b82bcacd6d0e
Manny-Ventura <wow.mannyv@gmail.com>
2022-06-30 23:04:26 -0400

    Configured routes files to incorporate resources method. tied it to :articles

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  resources :articles
   # Defines the root path route ("/")
 end
